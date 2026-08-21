129c2893104dc7020e9791544938b97c3341a69e
NischalRuchal <nischalruchal90@gmail.com>
2026-08-21 09:39:55 +0545

    Added nexus

--- FrontEnd/src/assets/assets.js
+++ FrontEnd/src/assets/assets.js
@@ -6,7 +6,7 @@ import Sunya from "./Sunya.png"
 import social from "./socialMedia.png"
 import spotify from "./spotify.png"
 import ghumtey from "./Ghumtey.png"
-import nexus from "./Nexus.png"
+import nexus from "./nexus.png"
 
 export const img = {
     sketch,
